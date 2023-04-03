b3136bf0e7a94b9c7c95250967f3193aa7ba8e58
Marc Friedenberg <maf946@psu.edu>
2023-04-03 13:33:34 -0400

    Initial commit

--- SubnetCalculator/IPaddressModule.py
+++ SubnetCalculator/IPaddressModule.py
@@ -1,35 +1,11 @@
+## this was me experimenting with the ipaddress module
+
 import ipaddress
 from random import getrandbits
 from random import choice
 import random
 from ipaddress import IPv4Network, IPv4Address
-
-
-# from netaddr import *
-# import pprint
-# def ip2bin(ip):
-#     octets = map(int, ip.split('/')[0].split('.')) # '1.2.3.4'=>[1, 2, 3, 4]
-#     binary = '{0:08b}{1:08b}{2:08b}{3:08b}'.format(*octets)
-#     range = int(ip.split('/')[1]) if '/' in ip else None
-#     return binary[:range] if range else binary
-#
-#
-# addr = ipaddress.ip_address('192.0.2.1')
-# print(ip2bin(str(addr)))
-#
-# exit()
-#
-#
-# ip = IPNetwork('172.16.0.0/12')
-# pp = pprint.PrettyPrinter()
-# pp.pprint("ip.cidr = %s" % ip.cidr)
-# pp.pprint("ip.first.ip = %s" % ip[0])
-# pp.pprint("ip.last.ip = %s" % ip[-1])
-#
-# ## DEBUG
-# exit()
-# ## DEBUG
-
+from enum import Enum
 
 def printRow(label, value):
     print('{:<30s}{:<5s}'.format(label + ":", str(value)))
@@ -41,42 +17,35 @@ def returnIP(ip):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     # determine public or private
+    class AddressType(Enum):
+        Public = 0
+        Private = 1
     publicOrPrivate = choice(range(0, 2))  # 0 means public, 1 means private
 
-
-
-    ## DEBUG
-    publicOrPrivate = 0
-
-    if (publicOrPrivate == 0):
+    if (AddressType(publicOrPrivate).name) == "Public":
         # we want to pick a random public IP
 
         while True:
             addr = IPv4Address(getrandbits(32))
             if (addr.is_global):
                 break
-
-
+        print(addr)
     ## code for public addresses
     else:
         # we want to pick a random private IP
-
         privateRangeFamily = choice(range(0, 3))
-
-        match privateRangeFamily:
-            case 0:
+        if (privateRangeFamily == 0):
                 printRow('Range', "Private (Class A)")
                 subnet = IPv4Network('10.0.0.0/24')
-            case 1:
-                printRow('Range', "Private (Class B)")
-                subnet = IPv4Network('172.16.0.0/12')
-            case 2:
-                printRow('Range', "Private (Class C)")
-                subnet = IPv4Network('192.168.0.0/16')
-            case _:
-                print("Something weird has happened")
+        elif (privateRangeFamily == 1):
+            printRow('Range', "Private (Class B)")
+            subnet = IPv4Network('172.16.0.0/12')
+        elif (privateRangeFamily == 2):
+            printRow('Range', "Private (Class C)")
+            subnet = IPv4Network('192.168.0.0/16')
+        else:
+            print("Something weird has happened")
 
         bits = getrandbits(subnet.max_prefixlen - subnet.prefixlen)
         firstAddress = subnet[1]
