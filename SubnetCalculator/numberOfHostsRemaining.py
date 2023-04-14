5edf8e0bcf2522ce0048fe82dd4441e73da44365
Marc Friedenberg <maf946@psu.edu>
2023-04-14 09:34:43 -0400

    Refactoring

--- SubnetCalculator/numberOfHostsRemaining.py
+++
@@ -1,5 +0,0 @@
-def numberOfHostsRemaining(count, numOfBitsNeed):
-    totalSpaceNeeded = 2 ** numOfBitsNeed
-    numHostsRemaining = (int(count) - totalSpaceNeeded)
-    print("You have " + str(numHostsRemaining) + " hosts remaining")
-    return numHostsRemaining
